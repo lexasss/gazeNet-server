4957328fec8505f0b73c81b7d3cb164eb9a74833
oleg.spakov <oleg.spakov@uta.fi>
2016-08-30 21:15:34 +0300

    sample logging removed

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
 #!/usr/bin/env node
 'use strict';
 
-//var app = require( '../app' );
 const config = require( './config/package' );
 const roles = require( './config/roles' );
 //var Records = require('../models/records');
 const WebSocketServer = require( 'ws' ).Server;
 const EventEmitter = require( 'events' ).EventEmitter;
-//var debug = require('debug')(process.env.npm_package_name);
 
 const messageBus = new EventEmitter();
 messageBus.setMaxListeners(50);
 
 //var sessions = app.get('sessions');
-
-/*
-var server = app.listen( env.web.port, undefined, undefined, function () {
-    //debug( 'Express server listening on port ' + server.address().port );
-    console.log( 'Express server ' + server.address().address + ' listening on port ' + server.address().port) ;
-});
-*/
 
 const wss = new WebSocketServer({ port: config.web.port });
 console.log( 'WS server listening on port ' + config.web.port ) ;
@@ -56,7 +47,6 @@ wss.on( 'connection', function (ws) {
             client.sources = [ ];
         }
 
-        //client.page = config.page;
         client.role = config.role ? config.role : roles.none;
         client.name = config.name ? config.name : id;
         
@@ -70,7 +60,6 @@ wss.on( 'connection', function (ws) {
 
     let parseEvent = function (data) {
         data.from = client.name;
-        print( 'event of ' + data.source + ' from ' + data.from + ' = ' + JSON.stringify( data.payload ) );
         messageBus.emit( 'message', data );
 
             /*
@@ -91,8 +80,6 @@ wss.on( 'connection', function (ws) {
         }
     };*/
 
-    // {"source": "default", "event": {"aaa": "value", "bbb": 111 }}
-
     var wsListener = function (data) {
         if (!(client.role & roles.receiver)) {
             return;
@@ -104,7 +91,6 @@ wss.on( 'connection', function (ws) {
 
         var sources = client.sources;
         if (sources && sources.indexOf( data.source ) >= 0) {
-            print( 'ws.send: ' + JSON.stringify( data.payload ) );
             ws.send( JSON.stringify({
                 payload: data.payload,
                 from: data.from
@@ -117,7 +103,6 @@ wss.on( 'connection', function (ws) {
     messageBus.on( 'message', wsListener );
 
     ws.on( 'message', function (data) {
-        print( 'Received: ' + data );
         let json; 
         try {
             json = JSON.parse( data );
@@ -132,7 +117,7 @@ wss.on( 'connection', function (ws) {
         } else if (json.config) {
             parseConfig( json.config );
         } else {
-            print( '?' );
+            print( 'Received unclassified data' );
         }
     });
 
